bbf72fe11f2c369adc3f1dad2641472438db5c22
HarunorRashed <mdharunanwer@gmail.com>
2025-05-22 10:38:32 +0600

    Create fixtures.jsx

---
+++ src/pages/fixtures.jsx
@@ -0,0 +1,10 @@
+function Fixtures() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold">Fixtures & Results</h2>
+      <p className="text-sm">Coming soon with submission feature</p>
+    </div>
+  );
+}
+
+export default Fixtures;
